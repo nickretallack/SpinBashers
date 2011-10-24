8230029f2619b3758b3ed0f5012cea71d34cd563
Nick Retallack <nickretallack@gmail.com>
2011-10-23 21:39:34 -0700

    unifying vector implementations

---
+++ vector_compatibility.coffee
@@ -0,0 +1,25 @@
+copy_methods = (here, there) ->
+    temp = {}
+    for name, method of here
+        continue if name == 'constructor'
+        temp[name] = method
+    for name, method of there
+        continue if name == 'constructor'
+        here[name] = method
+    for name, method of temp
+        continue if name == 'constructor'
+        there[name] = method
+
+#copy_methods b2Vec2.prototype, THREE.Vector2.prototype
+
+for name, method of b2Vec2.prototype
+    continue if name == 'constructor'
+    THREE.Vector2.prototype[name] = method
+
+Vector = window.b2Vec2 = THREE.Vector2
+Vector::components = -> [@x, @y]
+Vector::scale = (scalar) -> @clone().multiplyScalar scalar
+Vector::plus = (other) -> @clone().addSelf other
+Vector::minus = (other) -> @plus other.scale -1
+
+window.V = -> new Vector arguments...
---
+++ vector_compatibility.js
@@ -0,0 +1,58 @@
+(function() {
+  var Vector, copy_methods, method, name, _ref;
+  copy_methods = function(here, there) {
+    var method, name, temp, _results;
+    temp = {};
+    for (name in here) {
+      method = here[name];
+      if (name === 'constructor') {
+        continue;
+      }
+      temp[name] = method;
+    }
+    for (name in there) {
+      method = there[name];
+      if (name === 'constructor') {
+        continue;
+      }
+      here[name] = method;
+    }
+    _results = [];
+    for (name in temp) {
+      method = temp[name];
+      if (name === 'constructor') {
+        continue;
+      }
+      _results.push(there[name] = method);
+    }
+    return _results;
+  };
+  _ref = b2Vec2.prototype;
+  for (name in _ref) {
+    method = _ref[name];
+    if (name === 'constructor') {
+      continue;
+    }
+    THREE.Vector2.prototype[name] = method;
+  }
+  Vector = window.b2Vec2 = THREE.Vector2;
+  Vector.prototype.components = function() {
+    return [this.x, this.y];
+  };
+  Vector.prototype.scale = function(scalar) {
+    return this.clone().multiplyScalar(scalar);
+  };
+  Vector.prototype.plus = function(other) {
+    return this.clone().addSelf(other);
+  };
+  Vector.prototype.minus = function(other) {
+    return this.plus(other.scale(-1));
+  };
+  window.V = function() {
+    return (function(func, args, ctor) {
+      ctor.prototype = func.prototype;
+      var child = new ctor, result = func.apply(child, args);
+      return typeof result === "object" ? result : child;
+    })(Vector, arguments, function() {});
+  };
+}).call(this);
