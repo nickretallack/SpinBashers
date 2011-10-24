22f8db9be5e520cf17feba6a1e0194e2b421c63d
Nick Retallack <nickretallack@gmail.com>
2011-10-24 03:45:54 -0700

    got the rotation the way I want it

--- stuff.coffee
+++ stuff.coffee
@@ -11,6 +11,7 @@ $ ->
     torque = 50
     max_angular_velocity = 10
     default_size = V 1,1
+    default_color = 0xff0000
 
     world_size = V 2000, 2000
     world_padding = 50
@@ -28,14 +29,15 @@ $ ->
 
     sync_list = []
 
-    make_square = ({size, position, friction, dynamic}) ->
+    make_square = ({size, position, friction, dynamic, color}) ->
         size ?= default_size
         position ?= origin
         friction ?= default_friction
         dynamic ?= true
+        color ?= default_color
 
         geometry = new THREE.CubeGeometry size.components()..., 100
-        material = new THREE.MeshBasicMaterial color:0xff0000, wireframe:true
+        material = new THREE.MeshBasicMaterial color:color, wireframe:true
         mesh = new THREE.Mesh geometry, material
         mesh.position = position.three()
         scene.add mesh
@@ -52,6 +54,7 @@ $ ->
         body.CreateShape shape
         if dynamic
             body.SetMassFromShapes()
+            body.linearDamping = 1.0
 
         result =
             mesh:mesh
@@ -65,7 +68,7 @@ $ ->
 
     # graphics
     camera_radius = 10
-    camera = new THREE.OrthographicCamera camera_radius, -camera_radius,
+    camera = new THREE.OrthographicCamera -camera_radius, camera_radius,
          camera_radius, -camera_radius, -camera_radius, camera_radius
     camera.position.z = camera_radius * 2
     scene = new THREE.Scene()
@@ -82,27 +85,33 @@ $ ->
     world = new b2World world_box, gravity, do_sleep
     
     player1 = make_square
-        position:V(-5, -5)
+        position:V(-2, 2)
+        color:0x0000ff
     player2 = make_square
-        position:V(5, 5)
+        position:V(2, -2)
     
+    joint_definition = new b2DistanceJointDef()
+    joint_definition.Initialize player1.body, player2.body,
+        player1.body.GetPosition(), player2.body.GetPosition()
+    joint = world.CreateJoint joint_definition
 
     update = ->
-        ###
-        angular_velocity = player.body.GetAngularVelocity()
-        if pressed_keys.right and angular_velocity < max_angular_velocity
-            player.body.ApplyTorque torque
-            #player.body.ApplyForce new b2Vec2(-10,0), player.body.GetPosition()
-        if pressed_keys.left and angular_velocity > -max_angular_velocity
-            player.body.ApplyTorque -torque
-            #player.body.ApplyForce new b2Vec2(10,0), player.body.GetPosition()
-        if pressed_keys.space
-            player.body.ApplyImpulse V(10,0), player.body.GetPosition()
-        ###
+        force = 10
+        cardinals =
+            left:V(-1,0)
+            right:V(1,0)
+            up:V(0,1)
+            down:V(0,-1)
+
+        for key, direction of cardinals
+            if pressed_keys[key]
+                player1.body.ApplyForce direction.scale(force), player1.body.GetPosition()
 
         world.Step time_step, constraint_iterations
 
-        center = player1.body.GetPosition().plus player2.body.GetPosition()
+        player1_position = player1.body.GetPosition()
+        player2_position = player2.body.GetPosition()
+        center = player1_position.plus player2_position.minus(player1_position).scale(0.5)
         camera.position = center.three()
 
         for item in sync_list
--- stuff.js
+++ stuff.js
@@ -1,7 +1,7 @@
 (function() {
   var __slice = Array.prototype.slice;
   $(function() {
-    var animate, camera, camera_radius, constraint_iterations, default_friction, default_size, do_sleep, gravity, make_heap, make_level, make_square, max_angular_velocity, origin, player1, player2, player_friction, renderer, scene, sync_list, time_step, torque, update, window_size, world, world_box, world_padder, world_padding, world_size;
+    var animate, camera, camera_radius, constraint_iterations, default_color, default_friction, default_size, do_sleep, gravity, joint, joint_definition, make_heap, make_level, make_square, max_angular_velocity, origin, player1, player2, player_friction, renderer, scene, sync_list, time_step, torque, update, window_size, world, world_box, world_padder, world_padding, world_size;
     world_padder = V(world_padding, world_padding);
     origin = V(0, 0);
     window_size = function() {
@@ -15,6 +15,7 @@
     torque = 50;
     max_angular_velocity = 10;
     default_size = V(1, 1);
+    default_color = 0xff0000;
     world_size = V(2000, 2000);
     world_padding = 50;
     gravity = origin;
@@ -35,8 +36,8 @@
     };
     sync_list = [];
     make_square = function(_arg) {
-      var body, body_definition, dynamic, friction, geometry, material, mesh, position, result, shape, size;
-      size = _arg.size, position = _arg.position, friction = _arg.friction, dynamic = _arg.dynamic;
+      var body, body_definition, color, dynamic, friction, geometry, material, mesh, position, result, shape, size;
+      size = _arg.size, position = _arg.position, friction = _arg.friction, dynamic = _arg.dynamic, color = _arg.color;
             if (size != null) {
         size;
       } else {
@@ -57,13 +58,18 @@
       } else {
         dynamic = true;
       };
+            if (color != null) {
+        color;
+      } else {
+        color = default_color;
+      };
       geometry = (function(func, args, ctor) {
         ctor.prototype = func.prototype;
         var child = new ctor, result = func.apply(child, args);
         return typeof result === "object" ? result : child;
       })(THREE.CubeGeometry, __slice.call(size.components()).concat([100]), function() {});
       material = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
+        color: color,
         wireframe: true
       });
       mesh = new THREE.Mesh(geometry, material);
@@ -81,6 +87,7 @@
       body.CreateShape(shape);
       if (dynamic) {
         body.SetMassFromShapes();
+        body.linearDamping = 1.0;
       }
       result = {
         mesh: mesh,
@@ -94,7 +101,7 @@
       return result;
     };
     camera_radius = 10;
-    camera = new THREE.OrthographicCamera(camera_radius, -camera_radius, camera_radius, -camera_radius, -camera_radius, camera_radius);
+    camera = new THREE.OrthographicCamera(-camera_radius, camera_radius, camera_radius, -camera_radius, -camera_radius, camera_radius);
     camera.position.z = camera_radius * 2;
     scene = new THREE.Scene();
     renderer = new THREE.CanvasRenderer();
@@ -106,25 +113,34 @@
     do_sleep = true;
     world = new b2World(world_box, gravity, do_sleep);
     player1 = make_square({
-      position: V(-5, -5)
+      position: V(-2, 2),
+      color: 0x0000ff
     });
     player2 = make_square({
-      position: V(5, 5)
+      position: V(2, -2)
     });
+    joint_definition = new b2DistanceJointDef();
+    joint_definition.Initialize(player1.body, player2.body, player1.body.GetPosition(), player2.body.GetPosition());
+    joint = world.CreateJoint(joint_definition);
     update = function() {
-      /*
-              angular_velocity = player.body.GetAngularVelocity()
-              if pressed_keys.right and angular_velocity < max_angular_velocity
-                  player.body.ApplyTorque torque
-                  #player.body.ApplyForce new b2Vec2(-10,0), player.body.GetPosition()
-              if pressed_keys.left and angular_velocity > -max_angular_velocity
-                  player.body.ApplyTorque -torque
-                  #player.body.ApplyForce new b2Vec2(10,0), player.body.GetPosition()
-              if pressed_keys.space
-                  player.body.ApplyImpulse V(10,0), player.body.GetPosition()
-              */      var center, item, _i, _len;
+      var cardinals, center, direction, force, item, key, player1_position, player2_position, _i, _len;
+      force = 10;
+      cardinals = {
+        left: V(-1, 0),
+        right: V(1, 0),
+        up: V(0, 1),
+        down: V(0, -1)
+      };
+      for (key in cardinals) {
+        direction = cardinals[key];
+        if (pressed_keys[key]) {
+          player1.body.ApplyForce(direction.scale(force), player1.body.GetPosition());
+        }
+      }
       world.Step(time_step, constraint_iterations);
-      center = player1.body.GetPosition().plus(player2.body.GetPosition());
+      player1_position = player1.body.GetPosition();
+      player2_position = player2.body.GetPosition();
+      center = player1_position.plus(player2_position.minus(player1_position).scale(0.5));
       camera.position = center.three();
       for (_i = 0, _len = sync_list.length; _i < _len; _i++) {
         item = sync_list[_i];
