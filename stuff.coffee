165b44b0fb47356da52531ccbeba1418ff07ef49
Nick Retallack <nickretallack@gmail.com>
2011-10-23 22:45:03 -0700

    merged in stashed stuff about levels

--- stuff.coffee
+++ stuff.coffee
@@ -1,4 +1,23 @@
 $ ->
+    world_size = V 2000, 100
+    world_padding = 50
+    world_padder = V world_padding, world_padding
+    origin = V 0,0
+    window_size = -> V innerWidth, innerHeight
+    player_start = V 5,5
+    gravity = V 0.0, -10.0
+
+    make_heap = (location) ->
+        size = 1
+        elevation = 3
+        for index in [0..10]
+            make_square V(size, size), V(location, index*2), true
+
+    make_level = ->
+        make_heap 2
+
+    sync_list = []
+
     make_square = (size, position, dynamic) ->
         geometry = new THREE.CubeGeometry size.components()..., 100
         material = new THREE.MeshBasicMaterial color:0xff0000, wireframe:true
@@ -19,12 +38,15 @@ $ ->
         if dynamic
             body.SetMassFromShapes()
 
-        mesh:mesh
-        geometry:geometry
-        material:material
-        shape:shape
-        body:body
-        body_definition:body_definition
+        result =
+            mesh:mesh
+            geometry:geometry
+            material:material
+            shape:shape
+            body:body
+            body_definition:body_definition
+        sync_list.push result
+        result
 
     # graphics
     camera_radius = 10
@@ -33,7 +55,8 @@ $ ->
     camera.position.z = camera_radius * 2
     scene = new THREE.Scene()
     renderer = new THREE.CanvasRenderer()
-    renderer.setSize 300, 300
+    renderer.setSize 500, 500 #window.innerWidth, window.innerHeight
+    #renderer.setSize window.innerWidth, window.innerHeight
     document.body.appendChild renderer.domElement
 
     # physics
@@ -46,6 +69,18 @@ $ ->
     
     wall = make_square V(500, 1), V(0, -5)
     player = make_square V(1, 1), V(0, 0), true
+    make_level()
+
+    ###
+    world_box.lowerBound = origin.minus world_padder
+    world_box.upperBound = world_size.plus world_padder
+    do_sleep = true
+    world = new b2World world_box, gravity, do_sleep
+    
+    floor = make_square world_size.x, 1, origin.plus(V world_size.x,0).components()...
+    player = make_square 1, 1, player_start.components()..., true
+    make_level()
+    ###
 
     time_step = 1.0/60.0
     constraint_iterations = 10
@@ -61,8 +96,13 @@ $ ->
             #player.body.ApplyForce new b2Vec2(10,0), player.body.GetPosition()
 
         world.Step time_step, constraint_iterations
-        player.mesh.position = player.body.GetPosition().three()
-        player.mesh.rotation.z = player.body.GetAngle()
+
+        camera.position = player.body.GetPosition().three()
+
+        for item in sync_list
+            item.mesh.position = item.body.GetPosition().three()
+            item.mesh.rotation.z = item.body.GetAngle()
+
         renderer.render scene, camera
 
     animate = ->
--- stuff.js
+++ stuff.js
@@ -1,9 +1,32 @@
 (function() {
   var __slice = Array.prototype.slice;
   $(function() {
-    var animate, camera, camera_radius, constraint_iterations, do_sleep, gravity, make_square, player, renderer, scene, time_step, update, wall, world, world_box;
+    var animate, camera, camera_radius, constraint_iterations, do_sleep, gravity, make_heap, make_level, make_square, origin, player, player_start, renderer, scene, sync_list, time_step, update, wall, window_size, world, world_box, world_padder, world_padding, world_size;
+    world_size = V(2000, 100);
+    world_padding = 50;
+    world_padder = V(world_padding, world_padding);
+    origin = V(0, 0);
+    window_size = function() {
+      return V(innerWidth, innerHeight);
+    };
+    player_start = V(5, 5);
+    gravity = V(0.0, -10.0);
+    make_heap = function(location) {
+      var elevation, index, size, _results;
+      size = 1;
+      elevation = 3;
+      _results = [];
+      for (index = 0; index <= 10; index++) {
+        _results.push(make_square(V(size, size), V(location, index * 2), true));
+      }
+      return _results;
+    };
+    make_level = function() {
+      return make_heap(2);
+    };
+    sync_list = [];
     make_square = function(size, position, dynamic) {
-      var body, body_definition, geometry, material, mesh, shape;
+      var body, body_definition, geometry, material, mesh, result, shape;
       geometry = (function(func, args, ctor) {
         ctor.prototype = func.prototype;
         var child = new ctor, result = func.apply(child, args);
@@ -29,7 +52,7 @@
       if (dynamic) {
         body.SetMassFromShapes();
       }
-      return {
+      result = {
         mesh: mesh,
         geometry: geometry,
         material: material,
@@ -37,13 +60,15 @@
         body: body,
         body_definition: body_definition
       };
+      sync_list.push(result);
+      return result;
     };
     camera_radius = 10;
     camera = new THREE.OrthographicCamera(camera_radius, -camera_radius, camera_radius, -camera_radius, -camera_radius, camera_radius);
     camera.position.z = camera_radius * 2;
     scene = new THREE.Scene();
     renderer = new THREE.CanvasRenderer();
-    renderer.setSize(300, 300);
+    renderer.setSize(500, 500);
     document.body.appendChild(renderer.domElement);
     world_box = new b2AABB();
     world_box.lowerBound = V(-1000.0, -1000.0);
@@ -53,10 +78,21 @@
     world = new b2World(world_box, gravity, do_sleep);
     wall = make_square(V(500, 1), V(0, -5));
     player = make_square(V(1, 1), V(0, 0), true);
+    make_level();
+    /*
+        world_box.lowerBound = origin.minus world_padder
+        world_box.upperBound = world_size.plus world_padder
+        do_sleep = true
+        world = new b2World world_box, gravity, do_sleep
+        
+        floor = make_square world_size.x, 1, origin.plus(V world_size.x,0).components()...
+        player = make_square 1, 1, player_start.components()..., true
+        make_level()
+        */
     time_step = 1.0 / 60.0;
     constraint_iterations = 10;
     update = function() {
-      var torque;
+      var item, torque, _i, _len;
       torque = 8;
       if (pressed_keys.right) {
         player.body.ApplyTorque(torque);
@@ -65,8 +101,12 @@
         player.body.ApplyTorque(-torque);
       }
       world.Step(time_step, constraint_iterations);
-      player.mesh.position = player.body.GetPosition().three();
-      player.mesh.rotation.z = player.body.GetAngle();
+      camera.position = player.body.GetPosition().three();
+      for (_i = 0, _len = sync_list.length; _i < _len; _i++) {
+        item = sync_list[_i];
+        item.mesh.position = item.body.GetPosition().three();
+        item.mesh.rotation.z = item.body.GetAngle();
+      }
       return renderer.render(scene, camera);
     };
     animate = function() {
